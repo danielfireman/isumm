ed3485a25211fc4d6677b84c8ba53c84fbce08f0
Daniel Fireman <danielfireman@gmail.com>
2015-10-31 18:19:58 -0200

    First stab at op addition, performing validation and printing the import request paramenters.

--- handler_op.go
+++ handler_op.go
@@ -1,6 +1,29 @@
 package isumm
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
 func Op(w http.ResponseWriter, r *http.Request) {
+	strValue := r.FormValue("value")
+	if strValue == "" {
+		http.Error(w, "Value can not be empty.", http.StatusPreconditionFailed)
+		return
+	}
+	value, err := strconv.ParseFloat(strValue, 32)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Invalid value: %q", err), http.StatusPreconditionFailed)
+		return
+	}
+
+	inv := r.FormValue("inv")
+	if inv == "" {
+		http.Error(w, "Please select an investment.", http.StatusPreconditionFailed)
+		return
+	}
+	fmt.Fprintf(w, "inv:%s\n", inv)
+	fmt.Fprintf(w, "value:%f\n", value)
+	fmt.Fprintf(w, "type:%s\n", r.FormValue("type"))
 }
