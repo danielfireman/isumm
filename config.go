79064510f34eb5b9c23428dd247b2f00781c2919
Daniel Fireman <danielfireman@gmail.com>
2016-06-28 22:02:04 -0300

    letting marco access demo environment

--- config.go
+++ config.go
@@ -8,8 +8,9 @@ const (
 var AllowedUsers = map[string]struct{}{
 	"isumm.demo.staging@gmail.com": struct{}{},
 	"danielfireman@gmail.com":      struct{}{},
-	"contato@diasbruno.com":		struct{}{},
+	"contato@diasbruno.com":        struct{}{},
 	"idnotfound@gmail.com":         struct{}{},
+	"marcorosner@gmail.com":        struct{}{},
 }
 var AllowedTestUsers = map[string]struct{}{
 	"test@example.com": struct{}{},
