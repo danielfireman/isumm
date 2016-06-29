fea3ee45fe16ab86a97a5889719ddc956303a0e8
Bruno Dias <contato@diasbruno.com>
2016-06-29 00:33:58 -0300

    Merge branch 'master' into pr/56

--- config.go
+++ config.go
@@ -8,8 +8,9 @@ const (
 var AllowedUsers = map[string]struct{}{
 	"isumm.demo.staging@gmail.com": struct{}{},
 	"danielfireman@gmail.com":      struct{}{},
-	"contato@diasbruno.com":		struct{}{},
+	"contato@diasbruno.com":        struct{}{},
 	"idnotfound@gmail.com":         struct{}{},
+	"marco.rosner@gmail.com":        struct{}{},
 }
 var AllowedTestUsers = map[string]struct{}{
 	"test@example.com": struct{}{},
